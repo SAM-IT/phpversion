94d5d670c7de0d2979ca0a17ab5f9fac58f74bf7
Sam Mousa <sam@mousa.nl>
2016-07-18 14:35:32 +0000

    Removed indent

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -68,15 +68,15 @@ AppAsset::register($this);
     ?>
 
     <div class="container">
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- PHP Version Checker -->
-        <ins class="adsbygoogle"
-             style="display:inline-block;width:728px;height:90px"
-             data-ad-client="ca-pub-0873065610996616"
-             data-ad-slot="1868259251"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- PHP Version Checker -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-0873065610996616"
+     data-ad-slot="1868259251"></ins>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
